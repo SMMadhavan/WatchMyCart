34e0a7bb86011ffa5e8efe3753d8ed54c6095c59
SMMADHAVAN <smmadhavan9201@gmail.com>
2026-01-26 23:38:54 +0530

    Prompting Product URL

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from collector import get_product_data
 from storage import save_price
 from analysis import analyze_prices
 
+# Prompting Product URL
 url = input("Enter product URL: ")
 
 title, price = get_product_data(url)
