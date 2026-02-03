407574da3ce0fd57e7b1c059ebfdeec4bf1cb261
SMMADHAVAN <smmadhavan9201@gmail.com>
2026-02-03 14:39:59 +0530

    Updated main.py

--- src/main.py
+++ src/main.py
@@ -4,11 +4,10 @@ from analysis import analyze_prices, check_alert
 
 url = input("Enter product URL: ")
 target = float(input("Enter your target price: "))
+email = input("Enter your email: ")
 
 title, price = get_product_data(url)
 save_price(url, title, price)
 
-print("Recorded:", title, price)
-
 analyze_prices(url)
-check_alert(url, target)
+check_alert(url, target, email)
