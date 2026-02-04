86027da00fbc27bb4d5c6476a131b59f8591c99f
SMMADHAVAN <smmadhavan9201@gmail.com>
2026-02-04 20:52:10 +0530

    Added notifier.py

---
+++ src/notifier.py
@@ -0,0 +1,27 @@
+import smtplib
+from email.mime.text import MIMEText
+
+def send_email_alert(to_email, product, current, target):
+    from_email = "zzephyrr02@gmail.com"
+    app_password = "fidhofwxkfivjmti"
+
+    subject = "WatchMyCart Alert!"
+    body = f"""
+Price Alert Triggered!
+
+Product: {product}
+Current Price: {current}
+Target Price: {target}
+
+Time to buy.
+"""
+
+    msg = MIMEText(body)
+    msg["Subject"] = subject
+    msg["From"] = from_email
+    msg["To"] = to_email
+
+    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+    server.login(from_email, app_password)
+    server.send_message(msg)
+    server.quit()
