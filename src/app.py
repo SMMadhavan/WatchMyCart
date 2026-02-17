c73d0c4565b4a59e0e017ff680776616371b5b10
SMMADHAVAN <smmadhavan9201@gmail.com>
2026-02-17 19:22:52 +0530

    Updated app.py

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ from collector import get_product_data
 from storage import save_price
 from analysis import analyze_prices, check_alert
 
-#Flask Integration
+#Flask Integration for Backend
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
